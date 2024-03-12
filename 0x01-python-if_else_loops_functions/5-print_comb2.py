6f7282cdd9feb870f2b6afe8f617a303e89192cc
SemirMuhammed <semirmustafa22@gmail.com>
2024-03-12 21:37:14 +0300

    fix: usage of ord() in if condition - Task 7

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-for first_digit in range(10):
-    for last_digit in range(10):
-        if first_digit and last_digit == 9:
-            continue
-        print("{}{}".format(first_digit, last_digit), end=", ")
+for number in range(99):
+    print("{}{}".format("0" if number < 10 else "", number), end=", ")
 
 print(99)
